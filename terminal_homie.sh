10ad3ee1612b1300b1580ec8b3aa3e1183316b42
EngrMD <marcogdomingo@gmail.com>
2024-02-03 16:11:50 +0000

    terminal homie bash script added

---
+++ terminal_homie.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+greetings_file="$HOME/.terminal_greetings"
+
+if [ -e "$greetings_file" ]; then
+    num_lines=$(wc -l < "$greetings_file")
+    random_line=$((RANDOM % num_lines + 1))
+    selected_greeting=$(sed -n "${random_line}p" "$greetings_file")
+    echo "$selected_greeting"
+else
+    echo "File missing."
+fi
