d2be932388fc757c123c369ffd06de4ab98667da
Zab <maryelizabethchua@gmail.com>
2024-02-04 19:07:13 +0800

    add giga

--- terminal_homie.sh
+++ terminal_homie.sh
@@ -1,12 +1,17 @@
 #!/bin/zsh
 
+source terminal_homie_utils
 greetings_file="$HOME/.terminal_homie_greetings"
 
 if [ -e "$greetings_file" ]; then
-    num_lines=$(wc -l < "$greetings_file")
+    num_lines=$(( $(wc -l < "$greetings_file") + 1 ))
     random_line=$((RANDOM % num_lines + 1))
-    selected_greeting=$(sed -n "${random_line}p" "$greetings_file")
-    echo "$selected_greeting"
+    if [ "$random_line" = 1 ]; then
+        echo_gigachad "terminal_gigachad"
+    else
+        selected_greeting=$(sed -n "${random_line}p" "$greetings_file")
+        echo "$selected_greeting"
+    fi
 else
     echo "File missing."
 fi
