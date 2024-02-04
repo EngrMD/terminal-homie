3636ff70ddcefad307d22ce45f5edc407210a4b9
engrMD <marcogdomingo@gmail.com>
2024-02-04 14:59:30 +0000

    Successful installation displays gigachard art. Error in installation ends the script

--- install_terminal_greeting.sh
+++ install_terminal_greeting.sh
@@ -1,19 +1,10 @@
 #!/bin/zsh
 
-if [ -f ~/.terminal_homie_greetings ]; then
-    echo "Removing old installation: ~/.terminal_homie_greetings"
-    rm -f ~/.terminal_homie_greetings
-fi
-
-if [ -f ~/terminal_homie.sh ]; then
-    echo "Removing old installation: ~/terminal_homie.sh"
-    rm -f ~/terminal_homie.sh
-fi
+set -e
 
 chmod +x terminal_homie.sh
 
-cp -f ./terminal_homie_greetings ~/.terminal_homie_greetings
-cp -f ./terminal_homie.sh ~/terminal_homie.sh
+cp -f ./terminal_* $HOME/
 
 greeting_script_path=$(realpath ~/terminal_homie.sh)
 
@@ -21,6 +12,5 @@ if ! grep -qF "$greeting_script_path" ~/.zshrc; then
     echo "$greeting_script_path" >> ~/.zshrc
 fi
 
-source ~/.zshrc
-
-echo "Installation complete. Enjoy your greetings!"
+echo "Installation complete. Enjoy your terminal homie, GOAT!"
+cat terminal_gigachad
