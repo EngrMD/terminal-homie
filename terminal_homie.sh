30ae3d41481645fe58ee880e8dab529d7e2bf9ba
engrMD <marcogdomingo@gmail.com>
2024-02-04 15:01:17 +0000

    Terminal still greets when lucky gigachad appears

--- terminal_homie.sh
+++ terminal_homie.sh
@@ -1,13 +1,15 @@
 #!/bin/zsh
 
-source terminal_homie_utils
-greetings_file="$HOME/.terminal_homie_greetings"
+source terminal_homie_utils.sh
+greetings_file="$HOME/terminal_homie_greetings"
 
 if [ -e "$greetings_file" ]; then
     num_lines=$(( $(wc -l < "$greetings_file") + 1 ))
     random_line=$((RANDOM % num_lines + 1))
     if [ "$random_line" = 1 ]; then
         echo_gigachad "terminal_gigachad"
+        selected_greeting=$(sed -n "${random_line}p" "$greetings_file")
+        echo "\n ${selected_greeting}"
     else
         selected_greeting=$(sed -n "${random_line}p" "$greetings_file")
         echo "$selected_greeting"
